c97d23f6366f4268a999ef6ee325b629ce256040
AymanZuleyhi <149739736+AymanZuleyhi@users.noreply.github.com>
2025-06-17 15:54:28 +0300

    Your descriptive commit message here

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "*", // <--- TEMPORARY: This allows requests from any origin.
+    origin: "https://aymanzuleyhi.github.io", // <--- Change this back to your specific frontend URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
